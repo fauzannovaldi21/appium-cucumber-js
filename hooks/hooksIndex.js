6484a445f1a6c868922a9fa14c0af7748d028ae5
Fauzan Novaldi Suteja <fauzan.novaldi@dana.id>
2025-10-07 07:11:08 +0700

    removve uncomment

--- hooks/hooksIndex.js
+++ hooks/hooksIndex.js
@@ -37,29 +37,4 @@ After(async (scenario, result) => {
 
 AfterAll(async () => {
   console.log('✅ All tests completed');
-  // Report generated once in parent process (wdio.runner.js)
 });
-
-// /hooks/hooksIndex.js
-// import { Before, After, AfterAll, BeforeAll } from '@wdio/cucumber-framework'
-// import allureService from '../runner/services/allure.instance.js'
-
-// BeforeAll(async function() {
-//   console.log('🚀 Starting all test execution')
-//   allureService.attachMetadata()
-// })
-
-// Before(async function(world) {
-//   console.log(`🧩 Starting Scenario: ${world.pickle.name}`)
-// })
-
-// After(async function(world, result) {
-//   const status = result.result?.status || 'unknown'
-//   console.log(`🏁 Scenario finished: ${world.pickle.name}`)
-//   console.log(`🔹 Result: ${status}`)
-// })
-
-// AfterAll(async function() {
-//   console.log('✅ All tests completed — final cleanup')
-//   await cleanupAppSession()
-// })
